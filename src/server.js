ed9cf2a8be397802345348af61f605c86a01fb2c
Adolfo Cabeia <adolfojunior544@gmail.com>
2026-04-02 01:09:26 +0100

    api-zenith-formacao

--- src/server.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-import app from './app.js';
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-    console.log(`SERVIDOR RODANDO NA PORTA: http://localhost:${PORT}`);
-});
-//# sourceMappingURL=server.js.map
