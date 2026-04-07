962f9c8586da28de714245b62d56e084dac2aed3
Adolfo Cabeia <adolfojunior544@gmail.com>
2026-04-07 15:10:55 +0100

    up-date-7-15-10

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import express from 'express'
 import type { Request, Response } from "express";
 //import { GeminiAgent } from './mcp/ai/geminiAgents.js';
@@ -17,6 +19,7 @@ app.use(cors({
   origin: process.env.FRONTEND_URL,
   credentials: true
 }))
+
 app.use(express.json())
 
 app.get('/', (req:Request,resp:Response)=>{
